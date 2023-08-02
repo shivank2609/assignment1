f0294c79cf3df00151c7219f2875f03133f57c09
shivank2609 <141208327+shivank2609@users.noreply.github.com>
2023-08-02 11:12:42 +0530

    Add files via upload

---
+++ linked list.c
@@ -0,0 +1,107 @@
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+list_node* list_create(void *data)
+{
+	list_node *l = malloc(sizeof(list_node));
+	if (l != NULL) {
+		l->next = NULL;
+		l->data = data;
+	}
+
+	return l;
+}
+
+
+void list_destroy(list_node **list)
+{
+	if (list == NULL) return;
+	while (*list != NULL) {
+		list_remove(list, *list);
+	}
+}
+
+list_node* list_insert_after(list_node *node, void *data)
+{
+	list_node *new_node = list_create(data);
+	if (new_node) {
+		new_node->next = node->next;
+		node->next = new_node;
+	}
+	return new_node;
+}
+
+list_node* list_insert_beginning(list_node *list, void *data)
+{
+	list_node *new_node = list_create(data);
+	if (new_node != NULL) { new_node->next = list; }
+	return new_node;
+}
+
+list_node* list_insert_end(list_node *list, void *data)
+{
+	list_node *new_node = list_create(data);
+	if (new_node != NULL) {
+		for(list_node *it = list; it != NULL; it = it->next) {
+			if (it->next == NULL) {
+				it->next = new_node;
+				break;
+			}
+		}
+	}
+	return new_node;
+}
+
+void list_remove(list_node **list, list_node *node)
+{
+	list_node *tmp = NULL;
+	if (list == NULL || *list == NULL || node == NULL) return;
+
+	if (*list == node) {
+		*list = (*list)->next;
+		free(node);
+		node = NULL;
+	} else {
+		tmp = *list;
+		while (tmp->next && tmp->next != node) tmp = tmp->next;
+		if (tmp->next) {
+			tmp->next = node->next;
+			free(node);
+			node = NULL;
+		}
+	}
+}
+
+void list_remove_by_data(list_node **list, void *data)
+{
+	if (list == NULL || *list == NULL || data == NULL) return;
+	list_remove(list, list_find_by_data(*list, data));
+}
+list_node* list_find_node(list_node *list, list_node *node)
+{
+	while (list) {
+		if (list == node) break;
+		list = list->next;
+	}
+	return list;
+}
+
+list_node* list_find_by_data(list_node *list, void *data)
+{
+	while (list) {
+		if (list->data == data) break;
+		list = list->next;
+	}
+	return list;
+}
+list_node* list_find(list_node *list, int(*func)(list_node*,void*), void *data)
+{
+	if (!func) return NULL;
+	while(list) {
+		if (func(list, data)) break;
+		list = list->next;
+	}
+	return list;
+}
+
